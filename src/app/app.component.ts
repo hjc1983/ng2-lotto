c71314fb6aec24626a6cd881be9b29082b06c9fe
rc <hjc1983@gmail.com>
2016-07-13 21:22:42 +1000

    sample Http Get service with observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { LatestResultsComponent } from './latest-results/latest-results.component';
+import {Http, Response} from '@angular/http';
+
 
 @Component({
   moduleId: module.id,
@@ -10,4 +12,20 @@ import { LatestResultsComponent } from './latest-results/latest-results.componen
 })
 export class AppComponent {
   title = 'Home - app works!';
+data: Object;
+ loading: boolean;
+  constructor(public http: Http) {
+  }
+
+  makeRequest(): void {
+ // do something with this.http ...
+ this.loading = true;
+  this.http.request('https://thelott.com/sales/vmax/web/status') //this.http.request('http://jsonplaceholder.typicode.com/posts/1')
+ .subscribe((res: Response) => {
+ this.data = res.json();
+ this.loading = false;
+ });
+
+ }
+
 }
